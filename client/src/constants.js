72280123622c86b2c393789ff6ee4bb930da7c8b
ndTh4ng <thangxacam@gmail.com>
2022-01-13 21:19:54 +0700

    api url config

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,4 +3,4 @@ export const LOCAL_STORAGE_TOKEN_NAME = "chat-app";
 export const API_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "https:https://ndth4ng-chatapp.herokuapp.com/api";
+    : "https://ndth4ng-chatapp.herokuapp.com/api";
